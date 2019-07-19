c9e7922d832b83cef4a005d5cf903c984904580c
edvardjimenez <jim18009@byui.edu>
2019-07-20 01:46:58 +0800

    W11 Last Assignment
    Weatheria Final Submission.

---
+++ assignments/weathersite/scripts/townInformation.js
@@ -0,0 +1,44 @@
+var requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+var request = new XMLHttpRequest();
+request.open("GET", requestURL);
+request.responseType = "json";
+request.send();
+
+request.onload = function() {
+  var townInfo = request.response;
+  showData(townInfo);
+}
+
+function showData(jsonObj) {
+  var townData = jsonObj["towns"];
+
+  for (var i = 0; i < townData.length; i++) {
+    if (townData[i].name == "Preston" || townData[i].name == "Fish Haven" || townData[i].name == "Soda Springs") {
+      var string = "";
+      for (var j = 0; j < townData[i].name.length; j++) {
+        if (townData[i].name.charAt(j) != " ") {
+          string += townData[i].name.charAt(j);
+        }
+      }
+      console.log(string);
+      var townName = document.getElementById(string);
+
+      var myTown = document.createElement("h2");
+      myTown.textContent = townData[i].name;
+      townName.appendChild(myTown);
+
+      var myH3 = document.createElement("h3");
+      myH3.textContent = townData[i].motto;
+      townName.appendChild(myH3);
+
+      var myArticle = document.createElement("article");
+      townName.appendChild(myArticle);
+
+      var myPara = document.createElement("p");
+      myPara.innerHTML = "Year Founded: " + townData[i].yearFounded + "<br>" +
+                            "Population: " + townData[i].currentPopulation + "<br>" +
+                            "Average Rainfall: " + townData[i].averageRainfall;
+      myArticle.appendChild(myPara);
+    }
+  }
+}
---
+++ assignments/weathersite/scripts/weathersum.js
@@ -0,0 +1,25 @@
+let weatherRequest = new XMLHttpRequest();
+let apiURLstring =  'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=5943ce71a52d546135f1d898a9d7516c';
+weatherRequest.open('GET', apiURLstring, true);
+weatherRequest.send();
+
+weatherRequest.onload = function() {
+    let weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+
+    document.getElementById('weather-desc').innerHTML = weatherData.weather[0].description;
+    document.getElementById('weather-temp').innerHTML = weatherData.main.temp;
+    document.getElementById('weather-humidity').innerHTML = weatherData.main.humidity;
+    document.getElementById('weather-speed').innerHTML = weatherData.wind.speed;
+
+    let t = parseInt(document.getElementById('weather-temp').innerHTML);
+    let ws = parseInt(document.getElementById('weather-speed').innerHTML);
+
+    let f = 35.74 + (0.6215 * t) - (35.75 * Math.pow(ws, 0.16)) + (0.4275 * t * Math.pow(ws, 0.16));
+
+    var digits = 2;
+    var multiplier = Math.pow(10, digits);
+    f = Math.round(f * multiplier) / multiplier;
+
+    document.getElementById('wind-chill').innerHTML = f + ' ºF';
+}
