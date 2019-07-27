270ad61b02bd6f8295088e0f1789d4cc392b8e01
edvardjimenez <jim18009@byui.edu>
2019-07-27 14:03:51 +0800

    W14 Final Term Project

--- assignments/lesson-14/scripts/templeInformation.js
+++ assignments/lesson-14/scripts/templeInformation.js
@@ -35,24 +35,24 @@ function showData(jsonObj) {
         if (temples[i].name=="Manila Temple")
             {
                 myImg.setAttribute('src', 'images/temples/img-mnl-375.jpg');
-                myImg.setAttribute('srcset', 'images/temples/img-mnl-375.jpg 375w, images/temples/img-mnl-768.jpg 768w, images/temples/img-mnl-1024.jpg 1024w, images/temples/img-mnl-1440.jpg 1440w'); 
+                myImg.setAttribute('srcset', 'images/temples/img-mnl-375.jpg 375w, images/temples/img-mnl-768.jpg 768w, images/temples/img-mnl-1024.jpg 1024w, images/temples/img-mnl-1440.jpg 1440w');
                 myImg.setAttribute('alt', 'Manila Philippines Temple');
             }
         else if
             (temples[i].name=="Salt Lake Temple")
             {
                 myImg.setAttribute('src', 'images/temples/img-slc-375.jpg');
-                myImg.setAttribute('srcset', 'images/temples/img-slc-375.jpg 375w, images/temples/img-slc-768.jpg 768w, images/temples/img-slc-1024.jpg 1024w, images/temples/img-slc-1440.jpg 1440w'); 
+                myImg.setAttribute('srcset', 'images/temples/img-slc-375.jpg 375w, images/temples/img-slc-768.jpg 768w, images/temples/img-slc-1024.jpg 1024w, images/temples/img-slc-1440.jpg 1440w');
                 myImg.setAttribute('alt', 'Salt Lake City Utah Temple');
             }
         else if
             (temples[i].name=="Rexburg Idaho Temple")
             {
                 myImg.setAttribute('src', 'images/temples/img-idh-375.jpg');
-                myImg.setAttribute('srcset', 'images/temples/img-idh-375.jpg 375w, images/temples/img-idh-768.jpg 768w, images/temples/img-idh-1024.jpg 1024w, images/temples/img-idh-1440.jpg 1440w'); 
+                myImg.setAttribute('srcset', 'images/temples/img-idh-375.jpg 375w, images/temples/img-idh-768.jpg 768w, images/temples/img-idh-1024.jpg 1024w, images/temples/img-idh-1440.jpg 1440w');
                 myImg.setAttribute('alt', 'Rexburg Idaho Temple');
             }
-        else
+        else if
             (temples[i].name=="Rome Italy Temple")
             {
                 myImg.setAttribute('src', 'images/temples/img-rom-375.jpg');
@@ -60,19 +60,21 @@ function showData(jsonObj) {
                 myImg.setAttribute('alt', 'Rome Italy Temple');
             }
         
-        myH3.textContent = temples[i].name;
+        myH3.innerHTML = temples[i].name + '<br><br>';
         myH4.innerHTML = 
-            'Address:' + temples[i].address + "<br><br>" +
-            'Telephone:' + temples[i].telephone + "<br><br>" +
-            'Email:' + temples[i].email;
+            'Address: ' + temples[i].address + '<br>' +
+            'Telephone: ' + temples[i].telephone + '<br>' +
+            'Email: ' + temples[i].email + '<br>';
 
         myPara1.innerHTML =
-            'Services:' + temples[i].services + "<br><br>" +
-            'History:' + temples[i].history + "<br><br>" +
-            'Sessions:' + temples[i].session + "<br><br>" +
-            'Closures:' + temples[i].closure;
+            '<br><hr><br>' +
+            'Services: ' + temples[i].services + "<br><br>" +
+            'History: ' + temples[i].history + "<br><br>" +
+            'Sessions: ' + temples[i].session + "<br><br>" +
+            'Closures: ' + temples[i].closure;
         
-        myFigure.appendChild(myImg);   
+        myFigure.appendChild(myImg);
+        myFigure.appendChild(myFigCap); 
         myFigCap.appendChild(myH3);
         myFigCap.appendChild(myH4);
         myFigCap.appendChild(myPara1);
